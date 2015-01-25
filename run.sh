c468f455d138163a5f5c154d764cd6835e94173c
elijah grapes <ooisee@gmail.com>
2015-01-25 12:28:20 +0500

    detect platform

--- run.sh
+++
@@ -1,6 +0,0 @@
-convert 0 -gravity center -resize 256x -rotate 0 +repage -crop 256x192+0+0 ./evaluate/out_r0_s256_x0_y0.png
-convert 0 -gravity center -resize 257x -rotate 0 +repage -crop 256x192+0+0 ./evaluate/out_r0_s257_x0_y0.png
-convert 0 -gravity center -resize 258x -rotate 0 +repage -crop 256x192+0+0 ./evaluate/out_r0_s258_x0_y0.png
-convert 0 -gravity center -resize 259x -rotate 0 +repage -crop 256x192+0+0 ./evaluate/out_r0_s259_x0_y0.png
-convert 0 -gravity center -resize 260x -rotate 0 +repage -crop 256x192+0+0 ./evaluate/out_r0_s260_x0_y0.png
-convert 0 -gravity center -resize 261x -rotate 0 +repage -crop 256x192+0+0 ./evaluate/out_r0_s261_x0_y0.png
